46a055c14566a9992446c45c7297e53f9280c198
Saugat Deo <127115178+SaugatDeo@users.noreply.github.com>
2026-06-06 19:59:00 +0545

    Update app.py

--- app.py
+++ app.py
@@ -161,9 +161,9 @@ with st.sidebar:
     st.markdown("### 🫁 ChestAI")
     st.markdown("---")
     
-    api_url = st.text_input(
-        "API Endpoint",
-        value="http://127.0.0.1:8000",
+   api_url = st.text_input(
+    "API Endpoint",
+    value="https://saugatiwi-chest-xray-classifier.hf.space",
         help="FastAPI backend URL"
     )
     
